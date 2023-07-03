91756a5f86f3cd9cfc770b713aa03b729ddb9f77
Edson de Paula <109967193+edson-depaula@users.noreply.github.com>
2023-07-02 23:40:52 -0300

    Add files via upload

---
+++ Loja API/back-end/LojaApi/Models/Pipoca.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaApi.Models{
+
+    public class Pipoca{
+
+        [Key]
+        public int Id {get; set;}
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [MinLength(5, ErrorMessage = "O nome deve ter, pelo menos, 5 caracteres")]
+        public String Nome {get; set;}
+
+        public String Departamento {get; set;}
+
+        public String Generos {get; set;}
+
+        public DateTime DataCadastro {get; set;}
+
+        public String Apelido {get; set;}
+
+        public String Senha {get; set;}
+
+
+    }
+}
